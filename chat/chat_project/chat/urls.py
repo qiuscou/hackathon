ef2bb2b4a8d817e2e460bb822c8a5aff1343dcb9
Igor Krasnov <qiuscou@icloud.com>
2025-05-19 18:43:54 +0500

    Chat deleted

--- chat/chat_project/chat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'chat'
-
-urlpatterns = [
-    path('', views.index, name='index'),  # для /chat/
-    path('room/chat/', views.room, name='room'),  # для /chat/room_name/
-]
